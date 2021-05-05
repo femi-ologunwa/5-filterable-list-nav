76aa094da610a8f9cfbcb8a7b588bf3e5cb21e04
Femi <ologunwafemi@gmail.com>
2021-05-05 23:18:41 +0100

    created function to filter list items based on categories

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import items from './data';
 
 function App() {
    const [menuItems, setMenuItems] = useState(items);
+   const [categories, setCategories] = useState([]);
+
+   //function to filter list items based on category
+   const filterItems = (category) => {
+      if (category === 'all') {
+         setMenuItems(items);
+         return;
+      }
+      const newItems = items.filter((item) => item.category === category);
+      setMenuItems(newItems);
+   };
 
    return (
       <main>
@@ -13,6 +24,7 @@ function App() {
                <h2>our menu</h2>
                <div className='underline'></div>
             </div>
+            <Categories filterItems={filterItems} />
             <Menu items={menuItems} />
          </section>
       </main>
