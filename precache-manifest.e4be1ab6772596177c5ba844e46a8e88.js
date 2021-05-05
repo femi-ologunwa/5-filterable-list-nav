32f7d9a15a78030318fab434f8074e94fb0c2864
Femi <ologunwafemi@gmail.com>
2021-05-06 00:16:30 +0100

    Updates

---
+++ precache-manifest.e4be1ab6772596177c5ba844e46a8e88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8d2a44d97ad5b50c8284049eb2cfcc",
+    "url": "/5-filterable-list-nav/index.html"
+  },
+  {
+    "revision": "dc747dc9540429108ea5",
+    "url": "/5-filterable-list-nav/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/5-filterable-list-nav/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/5-filterable-list-nav/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc747dc9540429108ea5",
+    "url": "/5-filterable-list-nav/static/js/main.d599057a.chunk.js"
+  },
+  {
+    "revision": "92872f46cca7b0255306",
+    "url": "/5-filterable-list-nav/static/js/runtime-main.23d11c34.js"
+  }
+]);
